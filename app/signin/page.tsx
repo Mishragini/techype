7d636aa292dfffb3943d3fef889e865c587882e9
Mishragini <mishragini8@gmail.com>
2024-10-22 19:41:19 +0530

    buil fix

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -24,6 +24,7 @@ export default function SignIn() {
                 method: 'POST',
                 headers: {
                     'userid': userId!,
+                    'token': token!
                 },
             });
             Cookies.remove('token');
